7cd4f05076ad0d39a0a2a4ff78c25255f4eb92b7
Jonathan Donaldson <jdonaldson278@gmail.com>
2020-09-20 23:22:00 +0100

    Updated benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,53 +1,36 @@
-use base32h::{encode_binary, encode_binary_to_string, encode_to_string};
+use base32h::{decode_string, decode_string_to_binary, encode_binary_to_string, encode_to_string};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn encode(input: u128) -> String {
+    encode_to_string(input).unwrap()
+}
 
 fn encode_bin(input: &[u8]) -> String {
     encode_binary_to_string(input).unwrap()
 }
 
-fn encode_dig(input: u128) -> String {
-    encode_to_string(input).unwrap()
+fn decode(input: &str) -> u128 {
+    decode_string(input)
+}
+
+fn decode_bin(input: &str) -> Vec<u8> {
+    decode_string_to_binary(input)
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let text = b"hello world";
-    c.bench_function("Encode binary", |b| {
-        b.iter(|| encode_bin(black_box(&[255])))
-    });
-    c.bench_function("Encode text", |b| b.iter(|| encode_bin(black_box(text))));
+    c.bench_function("Encode digit", |b| b.iter(|| encode(black_box(4294967295))));
+    c.bench_function("decode digit", |b| b.iter(|| decode(black_box("3zZzZzZ"))));
 
-    c.bench_function("Large binary", |b| {
+    c.bench_function("encode binary", |b| {
         b.iter(|| {
             encode_bin(black_box(&[
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-                255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
             ]))
         })
     });
-    c.bench_function("Encode dig", |b| b.iter(|| encode_dig(black_box(255))));
+    c.bench_function("decode binary", |b| {
+        b.iter(|| decode_bin(black_box("zZzZzZzZzZzZzZzZ")))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
